c658aaf62e7c039376a252b590c3ac1420883925
A1va <1090034020@qq.com>
2021-04-26 14:01:39 +0800

    add

--- lib/ipc.c
+++ lib/ipc.c
@@ -1,24 +1,19 @@
-// User-level IPC library routines
+// 用户态IPC库程序
 
 #include "inc/lib.h"
 
-// Receive a value via IPC and return it.
-// If 'pg' is nonnull, then any page sent by the sender will be mapped at
-//	that address.
-// If 'from_env_store' is nonnull, then store the IPC sender's envid in
-//	*from_env_store.
-// If 'perm_store' is nonnull, then store the IPC sender's page permission
-//	in *perm_store (this is nonzero iff a page was successfully
-//	transferred to 'pg').
-// If the system call fails, then store 0 in *fromenv and *perm (if
-//	they're nonnull) and return the error.
-// Otherwise, return the value sent by the sender
-//
-// Hint:
-//   Use 'thisenv' to discover the value and who sent it.
-//   If 'pg' is null, pass sys_ipc_recv a value that it will understand
-//   as meaning "no page".  (Zero is not the right value, since that's
-//   a perfectly valid place to map a page.)
+/**
+ * 通过 IPC 接收一个值(或物理页)并返回值
+ * 如果'pg'为非null，则发送进程发送的任何物理页都将映射到该地址
+ * 如果'from_env_store'不为空，则将 IPC 发送进程的envid存储在*from_env_store中
+ * 如果'perm_store'不为null，则将IPC发送者的页面权限存储在*perm_store中(如果页面被成功传输到'pg'，则此权限为非零)
+ * 如果系统调用失败，那么将0存储在*fromenv和*perm中(如果它们是非null)并返回错误
+ * 否则，返回发送方发送的值
+ * 
+ * 使用‘thisenv’来发现值和发送进程
+ * 如果 pg 为空，则传递 sys_ipc_recv 一个值，它将理解为“no page”的意思
+ * (Zero 不是正确的值，因为这是映射页面的完美有效位置)
+ */
 int32_t
 ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 {
@@ -26,6 +21,7 @@ ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 	if(pg) {
 		r = sys_ipc_recv(pg);
 	}
+	// pg == 0, pg > UTOP
 	else {
 		r = sys_ipc_recv((void*)KERNBASE);
 	}
@@ -34,43 +30,47 @@ ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 		*perm_store = (perm_store != NULL) ? (int)0 : *perm_store;
 		return r;
 	}
+	// 设置from_env_store为发送进程的进程ID
 	if(from_env_store) {
 		*from_env_store = thisenv->env_ipc_from;
 	}
+	// 如果传递物理页，该位不为0，设置perm_store为物理页的权限
 	if(perm_store) {
 		*perm_store = thisenv->env_ipc_perm;
 	}
+	// 返回发送进程发送的值
 	return thisenv->env_ipc_value;
-	// LAB 4: Your code here.
-	//panic("ipc_recv not implemented");
 }
 
-// Send 'val' (and 'pg' with 'perm', if 'pg' is nonnull) to 'toenv'.
-// This function keeps trying until it succeeds.
-// It should panic() on any error other than -E_IPC_NOT_RECV.
-//
-// Hint:
-//   Use sys_yield() to be CPU-friendly.
-//   If 'pg' is null, pass sys_ipc_recv a value that it will understand
-//   as meaning "no page".  (Zero is not the right value.)
+/**
+ * 发送'val'(和'pg'带'perm'，如果'pg'是非null)给'toenv'
+ * 这个函数一直尝试，直到成功
+ * 除了-E_IPC_NOT_RECV之外的任何错误都应该panic()
+ * 
+ * 使用sys_yield()使cpu友好
+ * 如果'pg'为null，则给sys_ipc_recv传递一个它会理解为“无页面”的值(0不是正确的值)
+ */
 void
 ipc_send(envid_t to_env, uint32_t val, void *pg, int perm)
 {
 	int r = -E_IPC_NOT_RECV;
+	// 子进程不是接收状态
 	while(r == -E_IPC_NOT_RECV) {
 		if(pg) {
-			r = sys_ipc_try_send(to_env,val,pg,perm);
+			// 传递物理页
+			r = sys_ipc_try_send(to_env, val, pg, perm);
 		}
+		// pg == 0, pg > UTOP
 		else {
+			// (void*)KERNBASE > UTOP
 			r = sys_ipc_try_send(to_env, val, (void*)KERNBASE, perm);
 		}
+		// 让出CPU
 		sys_yield();
 	}
 	if (r != 0) {
 		panic("something went wrong with sending the page");
 	}
-	// LAB 4: Your code here.
-	//panic("ipc_send not implemented");
 }
 
 // Find the first environment of the given type.  We'll use this to
--- user/pingpong.c
+++ user/pingpong.c
@@ -1,5 +1,5 @@
-// Ping-pong a counter between two processes.
-// Only need to start one of these -- splits into two with fork.
+// 乒乓是两个进程之间的计数器.
+// 只需要开始其中的一个---- 用fork分成两个.
 
 #include "inc/lib.h"
 
@@ -9,17 +9,23 @@ umain(int argc, char **argv)
 	envid_t who;
 
 	if ((who = fork()) != 0) {
-		// get the ball rolling
+		// Parent 让球滚起来
+		// "send 0 from 父进程ID to 子进程ID"
 		cprintf("send 0 from %x to %x\n", sys_getenvid(), who);
+		// 仅发送值0到子进程，发现子进程尚未准备接收信息，让出CPU
 		ipc_send(who, 0, 0, 0);
 	}
 
 	while (1) {
+		// Child & Parent
+		// 置接收状态、目标线性地址、阻塞态、RAX返回值(成功)、让出CPU
+		// 调度、设置from发送进程ID(、设置perm_store)、返回发送进程发送的值
 		uint32_t i = ipc_recv(&who, 0, 0);
 		cprintf("%x got %d from %x\n", sys_getenvid(), i, who);
 		if (i == 10)
 			return;
 		i++;
+		// 仅发送值i到父进程，发现父进程尚未准备接收信息，让出CPU
 		ipc_send(who, i, 0, 0);
 		if (i == 10)
 			return;
